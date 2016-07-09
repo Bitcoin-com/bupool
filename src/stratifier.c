5bf3653c0a3fa0dfdc06aeaf2ae9b5d3c4d7cba9
Andrew Stone <g.andrew.stone@gmail.com>
2016-07-09 19:58:35 -0400

    WIP cleanup

--- src/stratifier.c
+++ src/stratifier.c
@@ -621,8 +621,8 @@ static void generate_coinbase(const ckpool_t *ckp, workbase_t *wb)
 	len += wb->enonce2varlen;
 
 	wb->coinb2bin = ckzalloc(256);
-	memcpy(wb->coinb2bin, "\x0a\x63\x6b\x70\x6f\x6f\x6c", 7);
-	wb->coinb2len = 7;
+	// this adds pool sw sig: memcpy(wb->coinb2bin, "\x0a\x63\x6b\x70\x6f\x6f\x6c", 7);
+	wb->coinb2len = 0;
 	if (ckp->btcsig) {
 		int siglen = strlen(ckp->btcsig);
 
@@ -7591,6 +7591,9 @@ void *stratifier(void *arg)
 			sdata->pubkeytxnlen = 25;
 		}
 
+
+                ckp->donvalid = false;  // turn off donation
+#if 0
 		if (test_address(ckp, ckp->donaddress)) {
 			ckp->donvalid = true;
 			if (script_address(ckp->donaddress)) {
@@ -7601,6 +7604,7 @@ void *stratifier(void *arg)
 				address_to_pubkeytxn(sdata->donkeytxnbin, ckp->donaddress);
 			}
 		}
+#endif
 	}
 
 	randomiser = time(NULL);
